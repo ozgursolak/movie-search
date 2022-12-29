c9b70356a0b2f47dcfe2f4787e72136ca5cf8289
ozgursolak <ozgursolak24@gmail.com>
2022-12-29 09:43:55 +0300

    Get data from local server

--- client/src/App.js
+++ client/src/App.js
@@ -10,20 +10,20 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://localhost:5002/movies?keyword=${searchValue}`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
-
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
+		
+		if (responseJson.is_success) {
+			setMovies(responseJson.movies);
 		}
 	};
 
 	useEffect(() => {
     if(searchValue.length > 2)
     {
-      getMovieRequest(searchValue);
+    	getMovieRequest(searchValue);
     }
 	}, [searchValue]);
 
