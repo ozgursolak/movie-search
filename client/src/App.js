b620553d3f8e943734314422f11167f3c98f4df1
ozgursolak <ozgursolak24@gmail.com>
2022-12-29 10:09:41 +0300

    Organize imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
